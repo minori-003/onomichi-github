764502c19b6381828095fe846daba58b77a97499
minori-003 <koishi86@gmail.com>
2025-06-16 16:58:22 +0900

    プライバシーのチェックボックスフォーカス時のアクションを追加

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+//ビューポートリサイズ
+(function () {
+  const viewport = document.querySelector('meta[name="viewport"]');
+  function switchViewport() {
+    const value =
+      window.outerWidth > 375
+        ? "width=device-width,initial-scale=1"
+        : "width=375";
+    if (viewport.getAttribute("content") !== value) {
+      viewport.setAttribute("content", value);
+    }
+  }
+  addEventListener("resize", switchViewport, false);
+  switchViewport();
+})();
+
 // ハンバーガーメニュー
 const drawerIcon = document.querySelector("#js-drawer-icon");
 const drawerContent = document.querySelector("#js-drawer-content");
